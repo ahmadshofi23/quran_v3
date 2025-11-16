7daed03807585dbb809cb64259a80ebc24a77910
ahmadshofi23 <ahmadshofihasibuan12@gmail.com>
2025-11-17 03:13:21 +0700

    update gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -17,31 +16,18 @@ if (keystoreFile.exists()) {
 android {
     namespace = "com.example.quran_v3"
     compileSdk = 35
-    ndkVersion = flutter.ndkVersion
+
     signingConfigs {
         create("release") {
-            storeFile = storeFile = file(keystoreProperties["storeFile"] ?: "app-key.jks")
-            storePassword = keystoreProperties["storePassword"] as String
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String? ?: "app-key.jks")
+            storePassword = keystoreProperties["storePassword"] as String? ?: ""
+            keyAlias = keystoreProperties["keyAlias"] as String? ?: ""
+            keyPassword = keystoreProperties["keyPassword"] as String? ?: ""
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.quran_v3"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = 35
         versionCode = flutter.versionCode
@@ -50,15 +36,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("release")
-        }
-    }
-
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
@@ -68,9 +46,6 @@ flutter {
 }
 
 dependencies {
-    // Tambahkan baris ini untuk desugaring library
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-
-    // Pastikan juga Kotlin stdlib tersedia
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
